3e7e168b523884c3074f23f8b45d3587b8cfea68
Muhammad Talha <talhariaz5425869@gmail.com>
2024-12-29 16:54:10 +0000

    sdfsd

--- about.js
+++ about.js
@@ -26,7 +26,7 @@ const about = async (client) => {
       const jid = "923185853847@s.whatsapp.net";
       // console.log("updatede");
       const urlofImage = await image(
-        "beautiful loneliness with black background"
+        "beautiful loneliness with black background ( no human ) "
       );
       console.log(urlofImage);
       const res = await axios.get(urlofImage, {
